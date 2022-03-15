5bd092d0577e9cb5f457da7dbb651219915514dd
tung <tommyonly0@gmail.com>
2022-03-15 11:56:21 +0700

    first commit

---
+++ app/src/main/java/com/example/laptrinhdidong/info.java
@@ -0,0 +1,43 @@
+package com.example.laptrinhdidong;
+
+
+import java.io.Serializable;
+
+
+
+public class info implements Serializable {
+    private String name;
+    private String email;
+    private String phone;
+
+    public info(String name, String email, String phone) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+}
